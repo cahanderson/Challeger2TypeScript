47d87740e64bc143dcd1c6dd5ea39eb0a6e78e65
Carlos Anderson <cahanderson@gmail.com>
2022-02-11 12:13:35 -0300

    Challenger2Part2

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,16 +1,16 @@
-import {
-  useEffect,
-  useRef,
-  useState,
-  useCallback,
-} from 'react';
-
+import {useEffect,useRef,useState,useCallback} from 'react';
 import { useField } from '@unform/core';
-
 import { Container } from './styles';
+import { IconBaseProps } from 'react-icons/lib';
 
-const Input = ({ name, icon: Icon, ...rest }) => {
-  const inputRef = useRef(null);
+interface Props{
+  name: string;
+  placeholder: string;
+  icon?:React.ComponentType<IconBaseProps>,
+}
+
+const Input = ({ name, icon: Icon, ...rest }:Props) => {
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
